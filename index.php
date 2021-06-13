eae036d9a9097de5b6537fafdd02afffe76f2779
Duong-Minh-Quang <1959032@itec.hcmus.edu.vn>
2021-06-13 15:24:32 +0700

    css change + login-box homepade

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include 'includes/header.php'; ?>
 
 <body>
-  <div class="container">
+  <div class="login-box jumbotron jumbotron-fluid text-center border border-secondary">
     <h2>Explore ur activity</h2>
     <h3>Get started for free</h3>
     <a href="#">Continue with Facebook</a><br>
@@ -9,20 +9,19 @@
     <h4>or</h4>
     <a href="#">Sign up here </a>
   </div>
+
   <br>
   <hr>
   <br>
-  <div class="fascination">
+  <div class="fascination container text-center ">
     <h2>Find what fasinates you</h2>
     <div class="row">
-      <a href="#">All categories</a>
-      <a href="#">Animation</a>
-      <a href="#">Design</a>
-      <a href="#">Illustration</a>
-      <a href="#">Lifestyle</a>
-      <a href="#">Photo & Film</a>
-      <a href="#">Business</a>
-      <a href="#">Writing</a>
+      <a href="#">All</a>
+      <a href="#">Martial arts</a>
+      <a href="#">Talents</a>
+      <a href="#">Survival skills</a>
+      <a href="#">Basic skills</a>
+      <a href="#">Arts</a>
     </div>
   </div>
 </body>
