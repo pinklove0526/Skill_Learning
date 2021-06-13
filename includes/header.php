42ffbe24d9f80b11ca5e0016aab5c1481e028260
Jellyfish Ng <pinklove0526@naver.com>
2021-06-13 14:32:05 +0700

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -7,9 +7,9 @@
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
         <link rel="stylesheet" href="css/style.css">
         <title>Skill Learning</title>
     </head>
-    <body> 
-
-            <h1>Hello world</h1>
+    <header>
+    </header>
