3601b2ba473ee9140e3fc16c0f5486beccab0637
Duong-Minh-Quang <1959032@itec.hcmus.edu.vn>
2021-06-13 16:41:51 +0700

    login/logout/sign_up adjust

--- includes/header.php
+++ includes/header.php
@@ -38,19 +38,19 @@
         <a class="nav-link" href="#">Arts</a>
       </li>
     </ul>
-    <ul class="navbar-nav float-right">
+    <ul class="account navbar-nav float-right">
                     <li class="nav-item active logout">
                       <a class="nav-link" href="logout.php"><i class="fa fa-door"></i> Logout<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item active signup">
                         <a class="nav-link" href="signup.php"><i class="fa fa-user">Sign-up</i> </a>
                     </li>
-                   
-                 
+
+
                     <li class="nav-item active login">
                         <a class="nav-link" href="login.php"><i class="fa fa-user"></i> Login<span class="sr-only">(current)</span></a>
                     </li>
-                
+
                 </ul>
   </div>
 </nav>
