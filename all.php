b2aac9cac9441bee4820016347f96bef68966d08
danielnhatng <1959026@itec.hcmus.edu.vn>
2021-06-28 15:40:11 +0700

    card

--- all.php
+++ all.php
@@ -7,4 +7,39 @@
     <img src="http://1.bp.blogspot.com/-aoLpZCDVDnA/VHrOvTNl0AI/AAAAAAAAEOk/dC9KbX1MBcw/s1600/NATURAL%2BLANDSCAPE.jpg" class="w-100" alt="">
   </div>
 </div>
+<div class="class-card">
+    <div class="row">
+        <div class="card-column col-md-4 col-sm-6">
+            <div class="card text-left">
+            <img class="card-img-top" src="holder.js/100px180/" alt="">
+            <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Body</p>
+            </div>
+            </div>
+        </div>
+
+        <div class="card-column col-md-4 col-sm-6">
+            <div class="card text-left">
+            <img class="card-img-top" src="holder.js/100px180/" alt="">
+            <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Body</p>
+            </div>
+            </div>
+        </div>
+
+        <div class="card-column col-md-4 col-sm-6">
+            <div class="card text-left">
+            <img class="card-img-top" src="holder.js/100px180/" alt="">
+            <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Body</p>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <?php include 'includes/footer.php'; ?>
