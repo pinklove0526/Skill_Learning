63c3ce3dcb148f4c357e6f47a223db78dabdd0fa
danielnhatng <1959026@itec.hcmus.edu.vn>
2021-06-28 15:51:35 +0700

    card

--- all.php
+++ all.php
@@ -7,7 +7,7 @@
     <img src="http://1.bp.blogspot.com/-aoLpZCDVDnA/VHrOvTNl0AI/AAAAAAAAEOk/dC9KbX1MBcw/s1600/NATURAL%2BLANDSCAPE.jpg" class="w-100" alt="">
   </div>
 </div>
-<<<<<<< HEAD
+
 <div class="class-card">
     <div class="row">
         <div class="card-column col-md-4 col-sm-6">
@@ -41,34 +41,4 @@
         </div>
     </div>
 </div>
-
-
-=======
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="https://images.allthingsnature.org/moss-on-stones-around-a-waterfall.jpg" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="https://images.allthingsnature.org/moss-on-stones-around-a-waterfall.jpg" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="https://images.allthingsnature.org/moss-on-stones-around-a-waterfall.jpg" alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
->>>>>>> 14d31f2eaded2145d9959a603824bb83bd2a8298
 <?php include 'includes/footer.php'; ?>
