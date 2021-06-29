ecf84790092e7387d67d462baabecec4d3b2e48f
Duong-Minh-Quang <1959032@itec.hcmus.edu.vn>
2021-06-29 15:58:07 +0700

    display classroom in all.php through classManager

--- func/classroom.php
+++ func/classroom.php
@@ -1,4 +1,23 @@
 <?php
+function outputPosts($classes) {
+  $output = '';
+  foreach ($classes as $class) {
+    $output.= "
+          <a href='#'>
+            <div class='card-column col-md-4 col-sm-6'>
+                <div class='card text-left'>
+                  <img class='card-img-top' src='' alt=''>
+                  <div class='card-body'>
+                      <a href='#'><h4 class='card-title'>$class['class_name']</h4></a>
+                      <p class='card-text'>$class['info']</p>
+                  </div>
+                </div>
+            </div>
+          </a>
 
+               ";
+  }
+  echo $output;
+}
 
 ?>
