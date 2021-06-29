88722fb063a239b2cab056059c97c53159806353
Duong-Minh-Quang <1959032@itec.hcmus.edu.vn>
2021-06-29 15:43:25 +0700

    display classes in all.php

--- all.php
+++ all.php
@@ -8,37 +8,113 @@
   </div>
 </div>
 
-<div class="class-card">
+<div class="class-card" >
     <div class="row">
         <div class="card-column col-md-4 col-sm-6">
             <div class="card text-left">
-            <img class="card-img-top" src="holder.js/100px180/" alt="">
+            <img class="card-img-top" src="https://images.hcmcpv.org.vn/res/news/2019/12/18-12-2019-khoi-tranh-giai-vo-dich-vovinam-the-gioi-nam-2019-108BA28-details.jpg?vs=18122019090921" alt="">
             <div class="card-body">
-                <h4 class="card-title">Title</h4>
-                <p class="card-text">Body</p>
+                <a href="#"><h4 class="card-title">Vovinam</h4></a>
+                <p class="card-text">Martial art</p>
             </div>
             </div>
         </div>
 
         <div class="card-column col-md-4 col-sm-6">
             <div class="card text-left">
-            <img class="card-img-top" src="holder.js/100px180/" alt="">
+            <img class="card-img-top" src="https://english.aawsat.com/sites/default/files/styles/article_img_top/public/2020/10/09/piano.jpg?itok=iuSIpVMi" alt="">
             <div class="card-body">
-                <h4 class="card-title">Title</h4>
-                <p class="card-text">Body</p>
+                <a href="#"><h4 class="card-title">Piano</h4></a>
+                <p class="card-text">Talent</p>
             </div>
             </div>
         </div>
 
         <div class="card-column col-md-4 col-sm-6">
             <div class="card text-left">
-            <img class="card-img-top" src="holder.js/100px180/" alt="">
+            <img class="card-img-top" src="https://www.idahomedicalacademy.com/wp-content/uploads/2019/08/First-Aid-Skills-Everyone-Should-Learn.png" alt="">
             <div class="card-body">
-                <h4 class="card-title">Title</h4>
-                <p class="card-text">Body</p>
+                <a href="#"><h4 class="card-title">First aid skill</h4></a>
+                <p class="card-text">Survival skills</p>
             </div>
             </div>
         </div>
+
+        <div class="card-column col-md-4 col-sm-6">
+            <div class="card text-left">
+            <img class="card-img-top" src="https://images.hcmcpv.org.vn/res/news/2019/12/18-12-2019-khoi-tranh-giai-vo-dich-vovinam-the-gioi-nam-2019-108BA28-details.jpg?vs=18122019090921" alt="">
+            <div class="card-body">
+                <a href="#"><h4 class="card-title">Vovinam</h4></a>
+                <p class="card-text">Martial art</p>
+            </div>
+            </div>
+        </div>
+
+
+    <div class="row">
+        <div class="card-column col-md-4 col-sm-6">
+            <div class="card text-left">
+            <img class="card-img-top" src="https://images.hcmcpv.org.vn/res/news/2019/12/18-12-2019-khoi-tranh-giai-vo-dich-vovinam-the-gioi-nam-2019-108BA28-details.jpg?vs=18122019090921" alt="">
+            <div class="card-body">
+                <a href="#"><h4 class="card-title">Vovinam</h4></a>
+                <p class="card-text">Martial art</p>
+            </div>
+            </div>
+        </div>
+
+        <div class="card-column col-md-4 col-sm-6">
+            <div class="card text-left">
+            <img class="card-img-top" src="https://english.aawsat.com/sites/default/files/styles/article_img_top/public/2020/10/09/piano.jpg?itok=iuSIpVMi" alt="">
+            <div class="card-body">
+                <a href="#"><h4 class="card-title">Piano</h4></a>
+                <p class="card-text">Talent</p>
+            </div>
+            </div>
+        </div>
+
+        <div class="card-column col-md-4 col-sm-6">
+            <div class="card text-left">
+            <img class="card-img-top" src="https://www.idahomedicalacademy.com/wp-content/uploads/2019/08/First-Aid-Skills-Everyone-Should-Learn.png" alt="">
+            <div class="card-body">
+                <a href="#"><h4 class="card-title">First aid skill</h4></a>
+                <p class="card-text">Survival skills</p>
+            </div>
+            </div>
+        </div>
+
+    <div class="row">
+        <div class="card-column col-md-4 col-sm-6">
+            <div class="card text-left">
+            <img class="card-img-top" src="https://images.hcmcpv.org.vn/res/news/2019/12/18-12-2019-khoi-tranh-giai-vo-dich-vovinam-the-gioi-nam-2019-108BA28-details.jpg?vs=18122019090921" alt="">
+            <div class="card-body">
+                <a href="#"><h4 class="card-title">Vovinam</h4></a>
+                <p class="card-text">Martial art</p>
+            </div>
+            </div>
+        </div>
+
+        <div class="card-column col-md-4 col-sm-6">
+            <div class="card text-left">
+            <img class="card-img-top" src="https://english.aawsat.com/sites/default/files/styles/article_img_top/public/2020/10/09/piano.jpg?itok=iuSIpVMi" alt="">
+            <div class="card-body">
+                <a href="#"><h4 class="card-title">Piano</h4></a>
+                <p class="card-text">Talent</p>
+            </div>
+            </div>
+        </div>
+
+        <div class="card-column col-md-4 col-sm-6">
+            <div class="card text-left">
+            <img class="card-img-top" src="https://www.idahomedicalacademy.com/wp-content/uploads/2019/08/First-Aid-Skills-Everyone-Should-Learn.png" alt="">
+            <div class="card-body">
+                <a href="#"><h4 class="card-title">First aid skill</h4></a>
+                <p class="card-text">Survival skills</p>
+            </div>
+            </div>
+        </div>
+
+
+
     </div>
 </div>
 <?php include 'includes/footer.php'; ?>
