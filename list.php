7a0b58d3e75452d3f5d64e042c4bc8252369d097
baolong2605 <1959015@itec.hcmus.edu.vn>
2021-07-05 21:08:08 +0700

    if user want to create class, log in first

--- list.php
+++ list.php
@@ -1,6 +1,8 @@
 <?php
 include 'includes/header.php';
 include 'classes/Classroom.php';
+
+
 if(isset($_POST['create_classroom'])) {
   $class_name = $_POST['class_name'];
   $class_type = $_POST['class_type'];
@@ -21,8 +23,18 @@ if(isset($_POST['create_classroom'])) {
   var_dump($_POST);
 }
 ?>
+
+
+
 <div class="container">
   <div class="row">
+      <?php if ($_SESSION['loggedin'] == false):?>
+          <div class="mt-5 col-md-6 offset-md-3 text-center">
+        <h2 class="display-5">Please Login to add classmate!</h2>
+        <p>Create an account or login to add classmate to the website.</p>
+        <button type="button" class="btn btn-block btn-outline-primary"><a href="login.php"><i class="fas fa-sign-in-alt"></i> Create Account/Login</a> </button>
+      </div>
+    <?php else: ?>
       <div class="mt-3 col-md-6 offset-md-3">
         <h2>Create class</h2>
         <div class="text-center">
@@ -62,14 +74,17 @@ if(isset($_POST['create_classroom'])) {
           <button type="submit" name="create_classroom" class="btn btn-outline-dark btn-block"> <i class="fas fa-edit"></i>Create</button>
         </form>
       </div>
+        <?php
+        
+     
+      
+       ?>
+    <?php endif; ?>
   </div>
 </div>
 <br>
 
 
-
-
-
 <?php
-include 'includes/footer.php'
+include 'includes/footer.php';
 ?>
