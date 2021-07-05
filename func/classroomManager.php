cdab0e476fd8560ee48e7c100153443f4b2f0a6e
baolong26052001 <80016176+baolong26052001@users.noreply.github.com>
2021-07-05 15:38:55 +0700

    Update classroomManager.php

--- func/classroomManager.php
+++ func/classroomManager.php
@@ -1,4 +1,23 @@
 <?php
+
+function checkClassroom($class_name, $class_type, $class_img, $info, $video, &$errors){
+  if($class_name == '' || $class_type == '' || $class_img == '' || $info == '' || $video == ''){
+    $errors['text'] = "You must fill in all fields!";
+  }
+}
+
+function createClassroom($class_name, $class_type, $class_img, $info, $video, $conn) {
+  $sql = "INSERT INTO classrooms (class_name, class_type, class_img, info, video) VALUES (?,?,?,?,?)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("sssss", $class_name, $class_type, $class_img, $info, $video);
+  $stmt->execute();
+  if($stmt->affected_rows == 1) {
+    // redirect user to the classmate they created
+    $location = "Location: classroom.php?id=" . $stmt->insert_id . "&created=true";
+    header($location);
+  }
+}
+
 function outputClasses($classes) {
   $output = '';
   foreach ($classes as $class) {
