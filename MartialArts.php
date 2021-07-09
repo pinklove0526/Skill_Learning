8d60bc2f985c70cf7f1148fe5765ae62ffb6db02
danielnhatng <1959026@itec.hcmus.edu.vn>
2021-07-09 15:34:14 +0700

    Create MartialArts.php

---
+++ MartialArts.php
@@ -0,0 +1,38 @@
+<?php
+  include 'includes/header.php';
+  include 'classes/Classroom.php';
+  include 'func/classroomManager.php';
+?>
+<style media="screen">
+  <?php include 'css/style.css'; ?>
+</style>
+<div class="container-fluid">
+  <div class="img-fluid">
+    <img src="http://1.bp.blogspot.com/-aoLpZCDVDnA/VHrOvTNl0AI/AAAAAAAAEOk/dC9KbX1MBcw/s1600/NATURAL%2BLANDSCAPE.jpg" class="w-100" alt="">
+  </div>
+  <!-- <?php if($_SESSION['loggedin'] == true): ?>
+    <h2 style="text-align: center;
+    margin: auto;">
+    Welcome aboard!</h2>
+    <h3>Hope u'll have a nice day</h3>
+    <h3>And take care of urself from the COVID-19!</h3>
+  <?php endif; ?> -->
+
+</div>
+  
+    <div class="container">
+      <h3>These are classrooms:</h3>
+      <hr>
+      <div class="row">
+            <?php
+        $classrooms = getClassrooms(12, $conn);
+
+        outputClassrooms($classrooms);
+      
+         ?>
+        <hr>
+       
+      </div>
+      <hr>
+    </div>
+<?php include 'includes/footer.php'; ?>
