646702c60fd6c5eda4b693cde402dc25e8e122f7
Jellyfish Ng <pinklove0526@naver.com>
2021-07-10 17:27:32 +0700

    Update skillshare.sql

--- skillshare.sql
+++ skillshare.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jul 09, 2021 at 08:33 AM
+-- Generation Time: Jul 10, 2021 at 10:25 AM
 -- Server version: 5.7.32
 -- PHP Version: 7.4.16
 
@@ -30,27 +30,32 @@ SET time_zone = "+00:00";
 CREATE TABLE `classroom` (
   `class_id` int(11) NOT NULL,
   `creator_id` int(11) DEFAULT NULL,
+  `class_type` text NOT NULL,
   `info` text NOT NULL,
   `class_name` text NOT NULL,
-  `class_img` text NOT NULL,
-  `video` text
+  `class_img` text,
+  `video` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `classroom`
 --
 
-INSERT INTO `classroom` (`class_id`, `creator_id`, `info`, `class_name`, `class_img`, `video`) VALUES
-(1, 1, 'Learn to fight like Bruce Lee!', 'Martial arts', '', 'https://www.youtube.com/embed/y9PkOR7kCrQ'),
-(2, 2, 'Shading make your character look better', 'Arts', '', 'https://www.youtube.com/embed/ZJkIaMECW6c'),
-(3, 3, 'Learn to survive in the wild', 'Survival skills', '', 'https://www.youtube.com/embed/fZndJO2jUJk'),
-(4, 4, 'Tips for life skills', 'Basic skills', '', 'https://www.youtube.com/embed/GOfAqfzGqhk'),
-(5, 5, 'Digital art for beginners', 'Arts', '', 'https://www.youtube.com/embed/iwRa5qTnr8o'),
-(6, 6, 'Upgrade water system', 'Survival skills', '', 'https://www.youtube.com/embed/F1GtNSKpCzg'),
-(7, 7, 'How to control fear before and during the fight', 'Martial arts', '', 'https://www.youtube.com/embed/03nXRY8JevI'),
-(8, 8, 'Tips and tricks for beginner plumbers', 'Basic skills', '', 'https://www.youtube.com/embed/LbtKUY77UPs'),
-(9, 9, 'Tips for character design', 'Arts', '', 'https://www.youtube.com/embed/v6lPsEPOIkM'),
-(10, 10, 'How to defend yourself when someone pushing you', 'Martial arts', '', 'https://www.youtube.com/embed/XARIQt1Z20M');
+INSERT INTO `classroom` (`class_id`, `creator_id`, `class_type`, `info`, `class_name`, `class_img`, `video`) VALUES
+(1, 1, '', 'Learn to fight like Bruce Lee !!!', 'Martial arts', '', ''),
+(2, 2, '', 'Shading makes your character look better', 'Arts', '', ''),
+(3, 3, '', 'Learn to survive in the wild', 'Survival skills', '', ''),
+(4, 4, '', 'Tips for life skills', 'Basic skills', '', ''),
+(5, 5, '', 'Digital art for beginners', 'Arts', '', ''),
+(6, 6, '', 'Upgrade water system', 'Survival skills', '', ''),
+(7, 7, '', 'How to control fear before and during the fight', 'Martial Arts', '', ''),
+(8, 8, '', 'Tips and tricks for beginner plumbers', 'Basic skills', '', ''),
+(9, 9, '', 'Tips for character design', 'Arts', '', ''),
+(10, 10, '', 'How to defend yourself when someone pushing you', 'Martial Arts', '', ''),
+(17, 11, 'martial arts', 'dfasdfsdaf', 'fsdfas', NULL, 'videos/60e96b4401e3e.mp4'),
+(18, 11, 'martial arts', 'fasdfsdfsdfasdf', 'fasdfsdfads', NULL, 'videos/60e96b748c6c8.mp4'),
+(19, 11, 'martial arts', 'dsafsdfasdfsd', 'fsadfasdfasdf', NULL, 'videos/60e96bb6d0b97.mp4'),
+(20, 11, 'martial arts', 'asdfadsf', 'fsadfasdf', NULL, 'videos/60e96eb86f9db.mp4');
 
 -- --------------------------------------------------------
 
@@ -89,7 +94,8 @@ INSERT INTO `users` (`ID`, `User_name`, `Email`, `hash`, `Avatar`) VALUES
 (3, 'Borny Hitch', 'HB@gmail.com', '$2y$10$KPzqlpPtAGLM0sCIKb.XKuPAZWTIGYTZ5s7fCXqUdKZOte7FiIzsm', ''),
 (4, 'Not_Bear_Grylls', 'NBG@gmail.com', '$2y$10$1tQvy8F6i1ydclX441yBUe5R6h1Sf4hu5q3ajt90kBJFBkMWtE.2S', ''),
 (5, 'Johnny Sin', 'JS@gmail.com', '$2y$10$77ud6NpSLoe45hUxlAgV9O0f0.5YuiLVNxMc9nEI25ZwH3oMxBOhu', ''),
-(6, 'Shindo L', 'ShindoL@gmail.com', '$2y$10$73uCIo/.eW/MDZHilMbuS.lxM/VnyJLIFF7p6HSbJri75Phlfb2Ry', '');
+(6, 'Shindo L', 'ShindoL@gmail.com', '$2y$10$73uCIo/.eW/MDZHilMbuS.lxM/VnyJLIFF7p6HSbJri75Phlfb2Ry', ''),
+(7, 'Joseph Joestar', 'bestjojo@gmail.com', '$2y$10$21nE4PEq.nlmqDPirpedeeZfWYBtwRh3zzzdKIks/JL6wewBzg5ma', '');
 
 --
 -- Indexes for dumped tables
@@ -121,7 +127,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `classroom`
 --
 ALTER TABLE `classroom`
-  MODIFY `class_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `class_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `comment`
@@ -133,7 +139,7 @@ ALTER TABLE `comment`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
