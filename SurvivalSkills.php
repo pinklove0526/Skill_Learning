e2e2a6bdc2983b8a6de2396ccd757e57603107b4
Jellyfish Ng <pinklove0526@naver.com>
2021-07-10 18:20:11 +0700

    Update SurvivalSkills.php

--- SurvivalSkills.php
+++ SurvivalSkills.php
@@ -24,7 +24,7 @@
       <hr>
       <div class="row">
             <?php
-        $classrooms = getClassrooms(12, $conn);
+        $classrooms = getSurvivalSkills(12, $conn);
         outputClassrooms($classrooms);
          ?>
         <hr>
