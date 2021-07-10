3bd6ec6d0e05dc816b1cad7d6b91ff1fda88f72f
danielnhatng <1959026@itec.hcmus.edu.vn>
2021-07-10 21:29:01 +0700

    Update skillshare.sql

--- skillshare.sql
+++ skillshare.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 10, 2021 at 02:05 PM
+-- Generation Time: Jul 10, 2021 at 02:28 PM
 -- Server version: 8.0.21
 -- PHP Version: 7.3.21
 
@@ -42,23 +42,23 @@ CREATE TABLE IF NOT EXISTS `classroom` (
 --
 
 INSERT INTO `classroom` (`class_id`, `class_type`, `info`, `class_name`, `video`) VALUES
-(1, 'Martial arts', 'Learn to fight like Bruce Lee !!!', 'Fighting 101\r\n', 'https://www.youtube.com/embed/y9PkOR7kCrQ'),
-(2, 'Arts', 'Shading makes your character look better', 'Draw anime 101', 'https://www.youtube.com/embed/ZJkIaMECW6c'),
-(3, 'Survival skills', 'Learn to survive in the wild', 'Live like Bear Grylls', 'https://www.youtube.com/embed/fZndJO2jUJk'),
-(4, 'Basic skills', 'Tips for life skills', 'How to basic', 'https://www.youtube.com/embed/GOfAqfzGqhk'),
-(5, 'Arts', 'Digital art for beginners', 'Digital Arts', 'https://www.youtube.com/embed/iwRa5qTnr8o'),
-(6, 'Survival skills', 'Upgrade water system', 'Better hygiene, better life', 'https://www.youtube.com/embed/F1GtNSKpCzg'),
-(7, 'Martial Arts', 'How to control fear before and during the fight', 'Self denfence', 'https://www.youtube.com/embed/03nXRY8JevI'),
-(8, 'Basic skills', 'Tips and tricks for beginner plumbers', 'Be a great plumber with Johnny Sins', 'https://www.youtube.com/embed/LbtKUY77UPs'),
-(9, 'Arts', 'Tips for character design', 'Design characters', 'https://www.youtube.com/embed/v6lPsEPOIkM'),
-(10, 'Martial Arts', 'How to defend yourself when someone pushing you', 'Make your life safe', 'https://www.youtube.com/embed/XARIQt1Z20M'),
+(1, 'Martial arts', 'Learn to fight like Bruce Lee !!!', 'Fighting 101\r\n', 'videos/60e96b4401e3e.mp4'),
+(2, 'Arts', 'Shading makes your character look better', 'Draw anime 101', 'videos/60e96b4401e3e.mp4'),
+(3, 'Survival skills', 'Learn to survive in the wild', 'Live like Bear Grylls', 'videos/60e96b4401e3e.mp4'),
+(4, 'Basic skills', 'Tips for life skills', 'How to basic', 'videos/60e96b4401e3e.mp4'),
+(5, 'Arts', 'Digital art for beginners', 'Digital Arts', 'videos/60e96b4401e3e.mp4'),
+(6, 'Survival skills', 'Upgrade water system', 'Better hygiene, better life', 'videos/60e96b4401e3e.mp4'),
+(7, 'Martial Arts', 'How to control fear before and during the fight', 'Self denfence', 'videos/60e96b4401e3e.mp4'),
+(8, 'Basic skills', 'Tips and tricks for beginner plumbers', 'Be a great plumber with Johnny Sins', 'videos/60e96b4401e3e.mp4'),
+(9, 'Arts', 'Tips for character design', 'Design characters', 'videos/60e96b4401e3e.mp4'),
+(10, 'Martial Arts', 'How to defend yourself when someone pushing you', 'Make your life safe', 'videos/60e96b4401e3e.mp4'),
 (20, 'martial arts', 'asdfadsf', 'fsadfasdf', 'videos/60e96eb86f9db.mp4'),
 (19, 'martial arts', 'dsafsdfasdfsd', 'fsadfasdfasdf', 'videos/60e96bb6d0b97.mp4'),
 (18, 'martial arts', 'fasdfsdfsdfasdf', 'fasdfsdfads', 'videos/60e96b748c6c8.mp4'),
 (17, 'martial arts', 'dfasdfsdaf', 'fsdfas', 'videos/60e96b4401e3e.mp4'),
-(21, 'Basic skills', 'Tips for life skills', '', 'https://www.youtube.com/embed/GOfAqfzGqhk'),
-(22, 'Basic skills', 'Tips for life skills', '', 'https://www.youtube.com/embed/GOfAqfzGqhk'),
-(23, 'Basic skills', 'Tips for life skills', '', 'https://www.youtube.com/embed/GOfAqfzGqhk');
+(21, 'Basic skills', 'Tips for life skills', '', 'videos/60e96b4401e3e.mp4'),
+(22, 'Basic skills', 'Tips for life skills', '', 'videos/60e96b4401e3e.mp4'),
+(23, 'Basic skills', 'Tips for life skills', '', 'videos/60e96b4401e3e.mp4');
 
 -- --------------------------------------------------------
 
