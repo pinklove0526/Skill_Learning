e5db32510c4e8d2b7ac4871e829a3f0e14d44f8a
danielnhatng <1959026@itec.hcmus.edu.vn>
2021-07-10 21:05:37 +0700

    Update skillshare.sql

--- skillshare.sql
+++ skillshare.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Jul 10, 2021 at 11:15 AM
--- Server version: 5.7.32
--- PHP Version: 7.4.16
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jul 10, 2021 at 02:05 PM
+-- Server version: 8.0.21
+-- PHP Version: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,35 +27,38 @@ SET time_zone = "+00:00";
 -- Table structure for table `classroom`
 --
 
-CREATE TABLE `classroom` (
-  `class_id` int(11) NOT NULL,
-  `creator_id` int(11) DEFAULT NULL,
+DROP TABLE IF EXISTS `classroom`;
+CREATE TABLE IF NOT EXISTS `classroom` (
+  `class_id` int NOT NULL AUTO_INCREMENT,
   `class_type` text NOT NULL,
   `info` text NOT NULL,
   `class_name` text NOT NULL,
-  `class_img` text,
-  `video` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `video` text NOT NULL,
+  PRIMARY KEY (`class_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `classroom`
 --
 
-INSERT INTO `classroom` (`class_id`, `creator_id`, `class_type`, `info`, `class_name`, `class_img`, `video`) VALUES
-(1, 1, 'martial arts', 'Learn to fight like Bruce Lee !!!', 'Martial arts', '', ''),
-(2, 2, 'arts', 'Shading makes your character look better', 'Arts', '', ''),
-(3, 3, 'survival skills', 'Learn to survive in the wild', 'Survival skills', '', ''),
-(4, 4, 'basic skills', 'Tips for life skills', 'Basic skills', '', ''),
-(5, 5, 'arts', 'Digital art for beginners', 'Arts', '', ''),
-(6, 6, 'survival skills', 'Upgrade water system', 'Survival skills', '', ''),
-(7, 7, 'martial arts', 'How to control fear before and during the fight', 'Martial Arts', '', ''),
-(8, 8, 'basic skills', 'Tips and tricks for beginner plumbers', 'Basic skills', '', ''),
-(9, 9, 'arts', 'Tips for character design', 'Arts', '', ''),
-(10, 10, 'martial arts', 'How to defend yourself when someone pushing you', 'Martial Arts', '', ''),
-(17, 11, 'martial arts', 'dfasdfsdaf', 'fsdfas', NULL, 'videos/60e96b4401e3e.mp4'),
-(18, 11, 'martial arts', 'fasdfsdfsdfasdf', 'fasdfsdfads', NULL, 'videos/60e96b748c6c8.mp4'),
-(19, 11, 'martial arts', 'dsafsdfasdfsd', 'fsadfasdfasdf', NULL, 'videos/60e96bb6d0b97.mp4'),
-(20, 11, 'martial arts', 'asdfadsf', 'fsadfasdf', NULL, 'videos/60e96eb86f9db.mp4');
+INSERT INTO `classroom` (`class_id`, `class_type`, `info`, `class_name`, `video`) VALUES
+(1, 'Martial arts', 'Learn to fight like Bruce Lee !!!', 'Fighting 101\r\n', 'https://www.youtube.com/embed/y9PkOR7kCrQ'),
+(2, 'Arts', 'Shading makes your character look better', 'Draw anime 101', 'https://www.youtube.com/embed/ZJkIaMECW6c'),
+(3, 'Survival skills', 'Learn to survive in the wild', 'Live like Bear Grylls', 'https://www.youtube.com/embed/fZndJO2jUJk'),
+(4, 'Basic skills', 'Tips for life skills', 'How to basic', 'https://www.youtube.com/embed/GOfAqfzGqhk'),
+(5, 'Arts', 'Digital art for beginners', 'Digital Arts', 'https://www.youtube.com/embed/iwRa5qTnr8o'),
+(6, 'Survival skills', 'Upgrade water system', 'Better hygiene, better life', 'https://www.youtube.com/embed/F1GtNSKpCzg'),
+(7, 'Martial Arts', 'How to control fear before and during the fight', 'Self denfence', 'https://www.youtube.com/embed/03nXRY8JevI'),
+(8, 'Basic skills', 'Tips and tricks for beginner plumbers', 'Be a great plumber with Johnny Sins', 'https://www.youtube.com/embed/LbtKUY77UPs'),
+(9, 'Arts', 'Tips for character design', 'Design characters', 'https://www.youtube.com/embed/v6lPsEPOIkM'),
+(10, 'Martial Arts', 'How to defend yourself when someone pushing you', 'Make your life safe', 'https://www.youtube.com/embed/XARIQt1Z20M'),
+(20, 'martial arts', 'asdfadsf', 'fsadfasdf', 'videos/60e96eb86f9db.mp4'),
+(19, 'martial arts', 'dsafsdfasdfsd', 'fsadfasdfasdf', 'videos/60e96bb6d0b97.mp4'),
+(18, 'martial arts', 'fasdfsdfsdfasdf', 'fasdfsdfads', 'videos/60e96b748c6c8.mp4'),
+(17, 'martial arts', 'dfasdfsdaf', 'fsdfas', 'videos/60e96b4401e3e.mp4'),
+(21, 'Basic skills', 'Tips for life skills', '', 'https://www.youtube.com/embed/GOfAqfzGqhk'),
+(22, 'Basic skills', 'Tips for life skills', '', 'https://www.youtube.com/embed/GOfAqfzGqhk'),
+(23, 'Basic skills', 'Tips for life skills', '', 'https://www.youtube.com/embed/GOfAqfzGqhk');
 
 -- --------------------------------------------------------
 
@@ -63,12 +66,14 @@ INSERT INTO `classroom` (`class_id`, `creator_id`, `class_type`, `info`, `class_
 -- Table structure for table `comment`
 --
 
-CREATE TABLE `comment` (
-  `comment_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `classroom_id` int(11) NOT NULL,
-  `body` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE IF NOT EXISTS `comment` (
+  `comment_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `classroom_id` int NOT NULL,
+  `body` text NOT NULL,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -76,13 +81,15 @@ CREATE TABLE `comment` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `ID` int NOT NULL AUTO_INCREMENT,
   `User_name` text NOT NULL,
   `Email` text NOT NULL,
   `hash` text NOT NULL,
-  `Avatar` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `Avatar` text,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `users`
@@ -90,56 +97,13 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`ID`, `User_name`, `Email`, `hash`, `Avatar`) VALUES
 (1, 'admin', 'admin@gmail.com', '$2y$10$UlffqcCPSVA.MxBuD0qjoOG/0p/BZdf4C9uWGsXHHx3v3keGCBZWa', ''),
-(2, 'Fighting god', 'godfight@gmail.com', '$2y$10$o2HXT6rBID0ADAkK.dwUde4U65r62wCfpoVSs.fQuvfIBTmwQgm1a', ''),
+(2, 'Figting god', 'godfight@gmail.com', '$2y$10$o2HXT6rBID0ADAkK.dwUde4U65r62wCfpoVSs.fQuvfIBTmwQgm1a', ''),
 (3, 'Borny Hitch', 'HB@gmail.com', '$2y$10$KPzqlpPtAGLM0sCIKb.XKuPAZWTIGYTZ5s7fCXqUdKZOte7FiIzsm', ''),
 (4, 'Not_Bear_Grylls', 'NBG@gmail.com', '$2y$10$1tQvy8F6i1ydclX441yBUe5R6h1Sf4hu5q3ajt90kBJFBkMWtE.2S', ''),
 (5, 'Johnny Sin', 'JS@gmail.com', '$2y$10$77ud6NpSLoe45hUxlAgV9O0f0.5YuiLVNxMc9nEI25ZwH3oMxBOhu', ''),
 (6, 'Shindo L', 'ShindoL@gmail.com', '$2y$10$73uCIo/.eW/MDZHilMbuS.lxM/VnyJLIFF7p6HSbJri75Phlfb2Ry', ''),
-(7, 'Joseph Joestar', 'bestjojo@gmail.com', '$2y$10$21nE4PEq.nlmqDPirpedeeZfWYBtwRh3zzzdKIks/JL6wewBzg5ma', '');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `classroom`
---
-ALTER TABLE `classroom`
-  ADD PRIMARY KEY (`class_id`);
-
---
--- Indexes for table `comment`
---
-ALTER TABLE `comment`
-  ADD PRIMARY KEY (`comment_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`ID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `classroom`
---
-ALTER TABLE `classroom`
-  MODIFY `class_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
-
---
--- AUTO_INCREMENT for table `comment`
---
-ALTER TABLE `comment`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+(7, 'Joseph Joestar', 'bestjojo@gmail.com', '$2y$10$21nE4PEq.nlmqDPirpedeeZfWYBtwRh3zzzdKIks/JL6wewBzg5ma', ''),
+(11, 'baolong', 'klasfhsjkld@gmail.com', '$2y$10$TIOJqRCuBRM9ruIgxLBnweu3zYCT16.EB/v0b0Kb1azbmH4/5wlBq', NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
