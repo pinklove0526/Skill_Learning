7ac1adda35a0451040dc69d565062ebbb4b22a5f
wolftheman001 <1959022@itec.hcmus.edu.vn>
2021-11-30 20:47:39 +0700

    asd

--- index.php
+++ index.php
@@ -1,61 +1,169 @@
 <?php
   include 'includes/header.php';
 ?>
-
+<style media="screen">
+  <?php include 'css/style.css'; ?>
+</style>
   <div class="login-box jumbotron jumbotron-fluid text-center border border-secondary" style="padding-bottom: 0px;">
     <img src="https://www.skillslearning.org/wp-content/uploads/2021/03/01..png" style="object-fit: contain; width: 100%; height: 100%">
   </div>
+  <div class="text-center">
+      <h1 class="p-3 display-3">Discover life long learning</h1>
+      <div class="p-5 row">
+      <div class="col-md-3">
+        <div class="card">
+          <img src="https://media.istockphoto.com/vectors/beautiful-nature-natural-landscape-evening-mountain-scenery-vector-id1167827469?k=20&m=1167827469&s=170667a&w=0&h=JEO0ZkZXE2VqRQ47io6NefR0GXQ7WkheZB_xkq3sq7I=" alt="">
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+          <br>
+          <h5>Bruh Samson</h5>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <img src="https://media.istockphoto.com/vectors/beautiful-nature-natural-landscape-evening-mountain-scenery-vector-id1167827469?k=20&m=1167827469&s=170667a&w=0&h=JEO0ZkZXE2VqRQ47io6NefR0GXQ7WkheZB_xkq3sq7I=" alt="">
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+          <br>
+          <h5>Bruh Samson</h5>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <img src="https://media.istockphoto.com/vectors/beautiful-nature-natural-landscape-evening-mountain-scenery-vector-id1167827469?k=20&m=1167827469&s=170667a&w=0&h=JEO0ZkZXE2VqRQ47io6NefR0GXQ7WkheZB_xkq3sq7I=" alt="">
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+          <br>
+          <h5>Bruh Samson</h5>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <img src="https://media.istockphoto.com/vectors/beautiful-nature-natural-landscape-evening-mountain-scenery-vector-id1167827469?k=20&m=1167827469&s=170667a&w=0&h=JEO0ZkZXE2VqRQ47io6NefR0GXQ7WkheZB_xkq3sq7I=" alt="">
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+          <br>
+          <h5>Bruh Samson</h5>
+        </div>
+      </div>
+      </div>
+  </div>
   <br>
   <br>
-  <div class="container text-center">
-    <div class="bg-1" style="
-        background-image: url(https://i.pinimg.com/originals/d5/7b/34/d57b3464ca4ac126ff2604ae517c4e96.jpg);
-        background-repeat: no-repeat;
-        background-size: cover;
-        color: #4A7CB5;">
-      <div class="row mb-5">
-        <div class="info col-md-4 col-lg-4 pr-1">
-          <h2>What will you discover?</h2>
-          <hr class="underline">
-          <h3>Explore new skills, deepen existing passions, and get lost in creativity. What you find just might surprise and inspire you.</h3>
-          <?php if($_SESSION['loggedin'] == false): ?>
-            <a href="login.php"><button type="button" class="btn btn-info">Log in to learn now!</button></a>
-          <?php endif; ?>
+  <div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://png.pngtree.com/thumb_back/fh260/background/20190827/pngtree-trendy-80s-abstract-geometric-background-image_304909.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h1 class="display-3">Journal for gratitude</h1>
+        <p>Make a commitment to your self-care and growth with self-healing advocate Yasmine Cheyenne.</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="https://image.freepik.com/free-vector/geometric-background-memphis-style_52683-35346.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h1 class="display-3">Make illustration your career</h1>
+        <p>Transform your artwork into products with illustrator Alicia Souza.</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="https://img.freepik.com/free-vector/elegant-dark-background-with-golden-details_23-2148413120.jpg?size=626&ext=jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h1 class="display-3">Introducing Chroma courses</h1>
+        <p>Accelerate your progress with new multi-week, immersive small group courses.</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+<div class="second-content">
+  <div class="row">
+  <div class="col-md-6">
+    <div class="text">
+    <h1>What did you discover?</h1>
+    <hr>
+    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, deserunt dolorem quisquam ipsum asperiores laborum iure ratione magnam architecto officiis cupiditate quae minus similique! Aliquam in enim blanditiis consequuntur harum?</h4>
+    <button class="btn btn-dark mt-3"><a href="all.php">Browse classes</a></button>
+    </div>
+  </div>
+  <div class="col-md-6">
+    <img src="images/Capture.PNG" alt="">
+  </div>
+</div>
+</div>
+<div class="second-content">
+  <div class="row">
+  <div class="col-md-6">
+    <img src="images/Capture.PNG" alt="">
+  </div>
+  <div class="col-md-6">
+    <div class="text">
+    <h1>Membership with meeting</h1>
+    <hr>
+    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, deserunt dolorem quisquam ipsum asperiores laborum iure ratione magnam architecto officiis cupiditate quae minus similique! Aliquam in enim blanditiis consequuntur harum?</h4>
+    <button class="btn btn-dark mt-3"><a href="all.php">Browse projects</a></button>
+    </div>
+  </div>
+</div>
+</div>
+<div class="second-content">
+  <div class="row">
+  <div class="col-md-6">
+    <div class="text">
+    <h1>Online learning design for real life</h1>
+    <hr>
+    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, deserunt dolorem quisquam ipsum asperiores laborum iure ratione magnam architecto officiis cupiditate quae minus similique! Aliquam in enim blanditiis consequuntur harum?</h4>
+    <button class="btn btn-dark mt-3"><a href="all.php">Try premium</a></button>
+    </div>
+  </div>
+  <div class="col-md-6">
+    <img src="images/Capture.PNG" alt="">
+  </div>
+</div>
+</div>
+<div class="text-center">
+      <h1 class="p-3 display-3">Classes taught by real creators</h1>
+      <hr>
+      <h3>They are experts and they are excited to share their wisdom and skills with you</h3>
+      <div class="p-5 row">
+      <div class="col-md-3">
+        <div class="card">
+          <img src="https://media.istockphoto.com/vectors/beautiful-nature-natural-landscape-evening-mountain-scenery-vector-id1167827469?k=20&m=1167827469&s=170667a&w=0&h=JEO0ZkZXE2VqRQ47io6NefR0GXQ7WkheZB_xkq3sq7I=" alt="">
+          <br>
+          <h4>Poosy Andie</h4>
+          <h5>Illustrator</h5>
         </div>
-        <img class="pic col-md-8 col-lg-8" src="https://blog.zoom.us/wp-content/uploads/2020/03/zoom-for-education.jpg" alt="">
       </div>
-      <div class="row mb-5">
-        <img class="pic col-md-8 col-lg-8" src="https://www.cedefop.europa.eu/files/images/identyfying_skills_needs_360x240.jpg" alt="">
-        <div class="info col-md-4 col-lg-4">
-          <h2>Membership With Meaning</h2>
-          <hr class="underline">
-          <h3>With so much to explore, real projects to create, and the support of fellow-creatives, Skillshare empowers you to accomplish real growth.</h3>
-          <?php if($_SESSION['loggedin'] == false): ?>
-            <a href="login.php"><button type="button" class="btn btn-info">Join to learn new skills</button></a>
-          <?php endif; ?>
+      <div class="col-md-3">
+        <div class="card">
+          <img src="https://media.istockphoto.com/vectors/beautiful-nature-natural-landscape-evening-mountain-scenery-vector-id1167827469?k=20&m=1167827469&s=170667a&w=0&h=JEO0ZkZXE2VqRQ47io6NefR0GXQ7WkheZB_xkq3sq7I=" alt="">
+          <br>
+          <h4>Bruh Samson</h4>
+          <h5>Artist</h5>
         </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <img src="https://media.istockphoto.com/vectors/beautiful-nature-natural-landscape-evening-mountain-scenery-vector-id1167827469?k=20&m=1167827469&s=170667a&w=0&h=JEO0ZkZXE2VqRQ47io6NefR0GXQ7WkheZB_xkq3sq7I=" alt="">
+          <br>
+          <h4>Ariyou Nuts</h4>
+          <h5>Designer</h5>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card">
+          <img src="https://media.istockphoto.com/vectors/beautiful-nature-natural-landscape-evening-mountain-scenery-vector-id1167827469?k=20&m=1167827469&s=170667a&w=0&h=JEO0ZkZXE2VqRQ47io6NefR0GXQ7WkheZB_xkq3sq7I=" alt="">
+          <br>
+          <h4>Oppsie Daisy</h4>
+          <h5>Writer</h5>
+        </div>
+      </div>
       </div>
   </div>
-  <div class="bg-2" style="
-      color: white;
-      background-image: url(https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__480.jpg);
-      background-repeat: no-repeat;
-      background-size: cover;
-      color: #e88aff;">
-      <div class="row mb-5">
-        <div class="info col-md-4 col-lg-4">
-          <h2>Classes Designed For Real Life</h2>
-          <hr class="underline">
-          <h3>Move your creative journey forward without putting life on hold. Skillshare helps you find inspiration that fits your routine.</h3>
-          <a href="login.php"><button type="button" class="btn btn-info">Are you a teacher? Click here to log in & create classroom</button></a>
-        </div>
-        <img class="pic col-md-8 col-lg-8" src="https://img.freepik.com/free-vector/people-enjoying-their-hobbies-their-places_52683-19472.jpg?size=626&ext=jpg" alt="">
-      </div>
-      <div class="row mb-5">
-
-
-      </div>
-  </div>
-  </div>
-
 <?php include 'includes/footer.php'; ?>
