3ac1cc47b09ba28a4848654943f307330bf7912f
baolong26052001 <80016176+baolong26052001@users.noreply.github.com>
2021-11-30 15:57:43 +0700

    Update post.php

--- post.php
+++ post.php
@@ -24,6 +24,7 @@ if(isset($_GET['id'])) {
        <div class="col-md-8 offset-md-2">
           
           <h2 class="font-weight-light mt-4"><?php echo htmlspecialchars($classroom['class_name']); ?></h2>
+          <h2 class="font-weight-light mt-4"><?php echo htmlspecialchars($classroom['contact_info']); ?></h2>
           <h2 class="font-weight-light mt-4"><?php echo htmlspecialchars($classroom['class_type']); ?></h2>
           <h5><em><?php echo htmlspecialchars($classroom['info']); ?></em></h5>
           <?php
