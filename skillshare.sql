9d86ec5d0ff3a88376b1ff963544f1c5c38e5308
danielnhatng <1959026@itec.hcmus.edu.vn>
2021-11-30 21:10:52 +0700

    Update skillshare.sql

--- skillshare.sql
+++ skillshare.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 10, 2021 at 02:28 PM
+-- Generation Time: Nov 30, 2021 at 02:09 PM
 -- Server version: 8.0.21
 -- PHP Version: 7.3.21
 
@@ -33,32 +33,41 @@ CREATE TABLE IF NOT EXISTS `classroom` (
   `class_type` text NOT NULL,
   `info` text NOT NULL,
   `class_name` text NOT NULL,
+  `contact_info` text NOT NULL,
+  `owner_name` text NOT NULL,
   `video` text NOT NULL,
   PRIMARY KEY (`class_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `classroom`
 --
 
-INSERT INTO `classroom` (`class_id`, `class_type`, `info`, `class_name`, `video`) VALUES
-(1, 'Martial arts', 'Learn to fight like Bruce Lee !!!', 'Fighting 101\r\n', 'videos/60e96b4401e3e.mp4'),
-(2, 'Arts', 'Shading makes your character look better', 'Draw anime 101', 'videos/60e96b4401e3e.mp4'),
-(3, 'Survival skills', 'Learn to survive in the wild', 'Live like Bear Grylls', 'videos/60e96b4401e3e.mp4'),
-(4, 'Basic skills', 'Tips for life skills', 'How to basic', 'videos/60e96b4401e3e.mp4'),
-(5, 'Arts', 'Digital art for beginners', 'Digital Arts', 'videos/60e96b4401e3e.mp4'),
-(6, 'Survival skills', 'Upgrade water system', 'Better hygiene, better life', 'videos/60e96b4401e3e.mp4'),
-(7, 'Martial Arts', 'How to control fear before and during the fight', 'Self denfence', 'videos/60e96b4401e3e.mp4'),
-(8, 'Basic skills', 'Tips and tricks for beginner plumbers', 'Be a great plumber with Johnny Sins', 'videos/60e96b4401e3e.mp4'),
-(9, 'Arts', 'Tips for character design', 'Design characters', 'videos/60e96b4401e3e.mp4'),
-(10, 'Martial Arts', 'How to defend yourself when someone pushing you', 'Make your life safe', 'videos/60e96b4401e3e.mp4'),
-(20, 'martial arts', 'asdfadsf', 'fsadfasdf', 'videos/60e96eb86f9db.mp4'),
-(19, 'martial arts', 'dsafsdfasdfsd', 'fsadfasdfasdf', 'videos/60e96bb6d0b97.mp4'),
-(18, 'martial arts', 'fasdfsdfsdfasdf', 'fasdfsdfads', 'videos/60e96b748c6c8.mp4'),
-(17, 'martial arts', 'dfasdfsdaf', 'fsdfas', 'videos/60e96b4401e3e.mp4'),
-(21, 'Basic skills', 'Tips for life skills', '', 'videos/60e96b4401e3e.mp4'),
-(22, 'Basic skills', 'Tips for life skills', '', 'videos/60e96b4401e3e.mp4'),
-(23, 'Basic skills', 'Tips for life skills', '', 'videos/60e96b4401e3e.mp4');
+INSERT INTO `classroom` (`class_id`, `class_type`, `info`, `class_name`, `contact_info`, `owner_name`, `video`) VALUES
+(1, 'Martial arts', 'Learn to fight like Bruce Lee !!!', 'Fighting 101\r\n', '', '', 'videos/60e96b4401e3e.mp4'),
+(2, 'Arts', 'Shading makes your character look better', 'Draw anime 101', '', '', 'videos/60e96b4401e3e.mp4'),
+(3, 'Survival skills', 'Learn to survive in the wild', 'Live like Bear Grylls', '', '', 'videos/60e96b4401e3e.mp4'),
+(4, 'Basic skills', 'Tips for life skills', 'How to basic', '', '', 'videos/60e96b4401e3e.mp4'),
+(5, 'Arts', 'Digital art for beginners', 'Digital Arts', '', '', 'videos/60e96b4401e3e.mp4'),
+(6, 'Survival skills', 'Upgrade water system', 'Better hygiene, better life', '', '', 'videos/60e96b4401e3e.mp4'),
+(7, 'Martial Arts', 'How to control fear before and during the fight', 'Self denfence', '', '', 'videos/60e96b4401e3e.mp4'),
+(8, 'Basic skills', 'Tips and tricks for beginner plumbers', 'Be a great plumber with Johnny Sins', '', '', 'videos/60e96b4401e3e.mp4'),
+(9, 'Arts', 'Tips for character design', 'Design characters', '', '', 'videos/60e96b4401e3e.mp4'),
+(10, 'Martial Arts', 'How to defend yourself when someone pushing you', 'Make your life safe', '', '', 'videos/60e96b4401e3e.mp4'),
+(20, 'martial arts', 'asdfadsf', 'fsadfasdf', '', '', 'videos/60e96eb86f9db.mp4'),
+(19, 'martial arts', 'dsafsdfasdfsd', 'fsadfasdfasdf', '', '', 'videos/60e96bb6d0b97.mp4'),
+(18, 'martial arts', 'fasdfsdfsdfasdf', 'fasdfsdfads', '', '', 'videos/60e96b748c6c8.mp4'),
+(17, 'martial arts', 'dfasdfsdaf', 'fsdfas', '', '', 'videos/60e96b4401e3e.mp4'),
+(21, 'Basic skills', 'Tips for life skills', '', '', '', 'videos/60e96b4401e3e.mp4'),
+(22, 'Basic skills', 'Tips for life skills', '', '', '', 'videos/60e96b4401e3e.mp4'),
+(23, 'Basic skills', 'Tips for life skills', '', '', '', 'videos/60e96b4401e3e.mp4'),
+(24, 'basic skills', 'az', 'asd', 'z', '', 'videos/61a5e99112c54.mp4'),
+(25, 'survival skills', 'za', 'as', 'asxz', '', 'videos/61a5e9a065322.mp4'),
+(26, 'survival skills', '0999999999', 'Survive', 'azaz', '', 'videos/61a62a8b53b6b.mp4'),
+(27, 'martial arts', '01224245', 'martial', 'as', '', 'videos/61a62ad471920.mp4'),
+(28, 'basic skills', '0218755123', 'damm', 'as', '', 'a'),
+(29, 'talents', 'facebook', 'Bad', 'tesasz', '', 'a'),
+(30, 'martial arts', 'insta', 'test2', 'asdsazxz', 'a', 'videos/61a62b988a2f4.mp4');
 
 -- --------------------------------------------------------
 
@@ -89,7 +98,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `hash` text NOT NULL,
   `Avatar` text,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `users`
@@ -103,7 +112,8 @@ INSERT INTO `users` (`ID`, `User_name`, `Email`, `hash`, `Avatar`) VALUES
 (5, 'Johnny Sin', 'JS@gmail.com', '$2y$10$77ud6NpSLoe45hUxlAgV9O0f0.5YuiLVNxMc9nEI25ZwH3oMxBOhu', ''),
 (6, 'Shindo L', 'ShindoL@gmail.com', '$2y$10$73uCIo/.eW/MDZHilMbuS.lxM/VnyJLIFF7p6HSbJri75Phlfb2Ry', ''),
 (7, 'Joseph Joestar', 'bestjojo@gmail.com', '$2y$10$21nE4PEq.nlmqDPirpedeeZfWYBtwRh3zzzdKIks/JL6wewBzg5ma', ''),
-(11, 'baolong', 'klasfhsjkld@gmail.com', '$2y$10$TIOJqRCuBRM9ruIgxLBnweu3zYCT16.EB/v0b0Kb1azbmH4/5wlBq', NULL);
+(11, 'baolong', 'klasfhsjkld@gmail.com', '$2y$10$TIOJqRCuBRM9ruIgxLBnweu3zYCT16.EB/v0b0Kb1azbmH4/5wlBq', NULL),
+(12, 'a', 'a@gmail.com', '$2y$10$WN9IV/RMakGKcWbM3VA1D.A21bYVRk0247jtmzrGsPMlvwS2JSxBq', NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
