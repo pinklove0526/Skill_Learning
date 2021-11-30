55831d4e4a5ac65267c45d26adb1cc1508e7ee1c
baolong26052001 <80016176+baolong26052001@users.noreply.github.com>
2021-11-30 16:18:32 +0700

    Update post.php

--- post.php
+++ post.php
@@ -22,11 +22,14 @@ if(isset($_GET['id'])) {
       </div>
      <?php else: ?>
        <div class="col-md-8 offset-md-2">
-          
-          <h2 class="font-weight-light mt-4"><?php echo htmlspecialchars($classroom['class_name']); ?></h2>
-          <h2 class="font-weight-light mt-4"><?php echo htmlspecialchars($classroom['contact_info']); ?></h2>
-          <h2 class="font-weight-light mt-4"><?php echo htmlspecialchars($classroom['class_type']); ?></h2>
-          <h5><em><?php echo htmlspecialchars($classroom['info']); ?></em></h5>
+         
+          <h2 class="font-weight-light mt-4">Class name: <?php echo htmlspecialchars($classroom['class_name']); ?></h2>
+         
+          <h2 class="font-weight-light mt-4">Contact info: <?php echo htmlspecialchars($classroom['contact_info']); ?></h2>
+         
+          <h2 class="font-weight-light mt-4">Class type: <?php echo htmlspecialchars($classroom['class_type']); ?></h2>
+         
+          <h5><em>Info: <?php echo htmlspecialchars($classroom['info']); ?></em></h5>
           <?php
           $output="
           <video width='360' height='200' controls>
