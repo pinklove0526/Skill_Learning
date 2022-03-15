9d14006408d9db0d5046cdc7241702de1a3f05f2
wolftheman001 <1959022@itec.hcmus.edu.vn>
2022-03-15 21:36:49 +0700

    post update

--- post.php
+++ post.php
@@ -1,61 +1,60 @@
 <?php
-include 'includes/header.php';
-include 'func/classroomManager.php';
-include 'classes/User.php';
+  include 'includes/header.php';
+  include 'func/classroomManager.php';
+  include 'classes/User.php';
 
-
-if(isset($_GET['id'])) {
-  $classroom = getClassroom($_GET['id'], $conn);
-  $theid = $_GET['id'];
- 
-}
-
- ?>
- <hr>
- <br>
- <br>
- <br>
- <div class="container post">
-   <div class="row">
+  if(isset($_GET['id'])) {
+    $classroom = getClassroom($_GET['id'], $conn);
+    $theid = $_GET['id'];
+  }
+?>
+<style media="screen">
+  <?php include 'css/style.css'; ?>
+</style>
+<div class="container mb-5 mt-5 d-flex">
+    <div class="col-md-3 class_icon">
+      <div class="circle bg-dark">
+        <i class="fas fa-user"></i>
+      </div>
+      <div class="rect mt-3 ml-3">
+        <h5>Creator's name</h5>
+      </div>
+      <div class="rect">
+        <h5>Number of students</h5>
+      </div>
+    </div>
+    <div class="col-md-6 class_info">
+      <div class="rect">
+        <h2>Classname</h2>
+        <hr>
+        <h4>Description</h4>
+        <!-- <h5>Number of students</h5> -->
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia at consequuntur cum sapiente nihil ut ipsa dolor dolorem alias ad sint quae enim totam exercitationem fugiat aliquam nesciunt, iusto error!</p>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <h3>Lesson:</h3>
+    <div class="rect">
+        <hr>
+    </div>
+    <div class="container post">
      <?php if ($classroom == false): ?>
        <h2 class="display-4">404 Post Not Found!</h2>
-      </div>
      <?php else: ?>
-       <div class="col-md-8 offset-md-2">
-         
-          <h2 class="font-weight-light mt-4">Class name: <?php echo htmlspecialchars($classroom['class_name']); ?></h2>
-         <h2 class="font-weight-light mt-4">Owner: <?php echo htmlspecialchars($classroom['owner_name']); ?></h2>
-         
-          <h2 class="font-weight-light mt-4">Contact info: <?php echo htmlspecialchars($classroom['contact_info']); ?></h2>
-         
-          <h2 class="font-weight-light mt-4">Class type: <?php echo htmlspecialchars($classroom['class_type']); ?></h2>
-         
-          <h5><em>Info: <?php echo htmlspecialchars($classroom['info']); ?></em></h5>
           <?php
-          $output="
-          <video width='360' height='200' controls>
-                  <source src='{$classroom['video']}' type='video/mp4'>
-                </video>
-          ";
+          $output="<video width='100%' controls>
+                    <source src='{$classroom['video']}' type='video/mp4'>
+                  </video>";
           echo $output;
           ?>
-          
-          
-          
        </div>
      </div> <!-- end of post row -->
-
-    
-     <?php
-
-      
-      ?>
      <?php endif; ?>
-
- </div>
-
- <hr>
- <?php
- 
- include 'includes/footer.php';
-  ?>
+     <div class="container">
+    <h3>Rating: 10/10</h3>
+    <textarea name="" id="" cols="30" rows="8" class="w-100" placeholder="Leave your comments here..."></textarea>
+    <button class="btn btn-dark comment mb-3" type="submit" name="post">Post</button>
+  </div>
+  </div>
+<?php include 'includes/footer.php'; ?>
