6b11750288e969ed55bbda5028758f64704b28d3
baolong26052001 <80016176+baolong26052001@users.noreply.github.com>
2022-03-26 15:52:36 +0700

    Update Rating.php

--- classes/Rating.php
+++ classes/Rating.php
@@ -1,15 +1,55 @@
 <?php
 class Rating
 {
-	public $rating_id;
-	public $class_id;
-	public $student_id;
-	public $rating_score;
+	public $RatingID;
+	public $ClassID;
+	public $StudentID;
+	public $RatingScore;
+    public $rating = [];
+    public $ratings = [];
 	public $conn;
 	
-	public function __construct($conn)
+	public function __construct($ClassID, $conn)
 	{
-		$this->conn = $conn;
+		$this->RatingClassID = $ClassID;
+        $this->conn = $conn;
 	}
+
+    public function getRatings()
+    {
+        $sql = "SELECT r.RatingID, r.ClassID, r.StudentID, r.RatingScore, u.User_name, u.ID
+        FROM rating r JOIN users u ON r.StudentID = u.ID
+        WHERE r.ClassID = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("i", $this->RatingClassID);
+        $stmt->execute();
+        $results = $stmt->get_result();
+        $this->ratings = $results->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function createRating()
+    {
+        $sql = "INSERT INTO rating (RatingID, ClassID, StudentID, RatingScore) VALUES (?, ?, ?, ?)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("iii", $_SESSION['user_id'], $this->RatingClassID, $this->RatingScore);
+        $stmt->execute();
+        if ($stmt->affected_rows == 1)
+        {
+            
+            $this->getRating();
+        }
+    }
+
+    public function getRating()
+    {
+        $sql = "SELECT r.RatingID, r.ClassID, r.StudentID, r.RatingScore, u.User_name, u.ID
+        FROM rating r JOIN users u ON r.StudentID = u.ID
+        WHERE r.ClassID = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("i", $this->RatingID);
+        $stmt->execute();
+        $results = $stmt->get_result();
+        $this->rating = $results->fetch_all(MYSQLI_ASSOC);
+    }
 }
 ?>
