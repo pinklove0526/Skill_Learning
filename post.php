a3711a2a3b3a589cd18bd4027ae51f3ab1d546c7
baolong26052001 <80016176+baolong26052001@users.noreply.github.com>
2022-04-07 17:54:11 +0700

    Update rating

--- post.php
+++ post.php
@@ -76,25 +76,27 @@
      <div class="rate">
     
     <input type="radio" id="star1" name="rate" value="1" />
-    <label for="star1" title="text">1 star</label>
+    <label for="star1" title="text">1 </label>
     <input type="radio" id="star2" name="rate" value="2" />
-    <label for="star2" title="text">2 stars</label>
+    <label for="star2" title="text">2 </label>
     <input type="radio" id="star3" name="rate" value="3" />
-    <label for="star3" title="text">3 stars</label>
+    <label for="star3" title="text">3 </label>
     <input type="radio" id="star4" name="rate" value="4" />
-    <label for="star4" title="text">4 stars</label>
+    <label for="star4" title="text">4 </label>
     <input type="radio" id="star5" name="rate" value="5" />
-    <label for="star5" title="text">5 stars</label>
+    <label for="star5" title="text">5 </label>
     <input type="radio" id="star5" name="rate" value="6" />
-    <label for="star6" title="text">6 stars</label>
+    <label for="star6" title="text">6 </label>
     <input type="radio" id="star5" name="rate" value="7" />
-    <label for="star7" title="text">7 stars</label>
+    <label for="star7" title="text">7 </label>
     <input type="radio" id="star5" name="rate" value="8" />
-    <label for="star8" title="text">8 stars</label>
+    <label for="star8" title="text">8 </label>
     <input type="radio" id="star5" name="rate" value="9" />
-    <label for="star9" title="text">9 stars</label>
+    <label for="star9" title="text">9 </label>
     <input type="radio" id="star5" name="rate" value="10" />
-    <label for="star10" title="text">10 stars</label>
+    <label for="star10" title="text">10 </label>
+    <br>
+    <button class="btn btn-dark comment mb-3" type="submit" name="rate">Rate</button>
   </div>
   <br>
   <br>
@@ -104,61 +106,3 @@
   </div>
   </div>
 <?php include 'includes/footer.php'; ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<style>
-  *{
-    margin: 0;
-    padding: 0;
-}
-.rate {
-    float: left;
-    height: 46px;
-    padding: 0 10px;
-}
-.rate:not(:checked) > input {
-    position:absolute;
-    top:-9999px;
-}
-.rate:not(:checked) > label {
-    float:right;
-    width:1em;
-    overflow:hidden;
-    white-space:nowrap;
-    cursor:pointer;
-    font-size:30px;
-    color:#ccc;
-}
-.rate:not(:checked) > label:before {
-    content: '★ ';
-}
-.rate > input:checked ~ label {
-    color: #ffc700;    
-}
-.rate:not(:checked) > label:hover,
-.rate:not(:checked) > label:hover ~ label {
-    color: #deb217;  
-}
-.rate > input:checked + label:hover,
-.rate > input:checked + label:hover ~ label,
-.rate > input:checked ~ label:hover,
-.rate > input:checked ~ label:hover ~ label,
-.rate > label:hover ~ input:checked ~ label {
-    color: #c59b08;
-}
-
-
-  </style>
