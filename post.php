b576ff554c0ec02a9f6ef480f13f5d6b4b04c0dd
baolong26052001 <80016176+baolong26052001@users.noreply.github.com>
2022-04-07 19:46:06 +0700

    rating

--- post.php
+++ post.php
@@ -88,15 +88,15 @@
     <label for="star4" title="text">4 </label>
     <input type="radio" id="star5" name="rate" value="5" />
     <label for="star5" title="text">5 </label>
-    <input type="radio" id="star5" name="rate" value="6" />
+    <input type="radio" id="star6" name="rate" value="6" />
     <label for="star6" title="text">6 </label>
-    <input type="radio" id="star5" name="rate" value="7" />
+    <input type="radio" id="star7" name="rate" value="7" />
     <label for="star7" title="text">7 </label>
-    <input type="radio" id="star5" name="rate" value="8" />
+    <input type="radio" id="star8" name="rate" value="8" />
     <label for="star8" title="text">8 </label>
-    <input type="radio" id="star5" name="rate" value="9" />
+    <input type="radio" id="star9" name="rate" value="9" />
     <label for="star9" title="text">9 </label>
-    <input type="radio" id="star5" name="rate" value="10" />
+    <input type="radio" id="star10" name="rate" value="10" />
     <label for="star10" title="text">10 </label>
     <br>
     <button class="btn btn-dark comment mb-3" type="submit" name="rate">Rate</button>
