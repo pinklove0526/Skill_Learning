a96671e8c108034c66ddebc2b6680e50260828a6
danielnhatng <1959026@itec.hcmus.edu.vn>
2022-04-09 21:21:53 +0700

    Material

--- classes/material.php
+++ classes/material.php
@@ -12,8 +12,26 @@ class material {
     public $conn;
     public $errors = [];
     public function __construct($conn) {
-        $this->conn = $conn;
+        $this->conn = $conn;      }
+    public function getMaterial() {
+      $sql = "SELECT * FROM 'material/quiz' WHERE ContentID = ?";
+      $stmt = $this->conn->prepare($sql);
+      $stmt->bind_param("s", $this->contentName);
+      $stmt->execute();
+      $results = $stmt->get_result();
+      if($results->num_rows == 1) {
+        $this->class = $results->fetch_assoc();
       }
-}
-
+    }
+    public function getMaterials($contentID) {
+      this->contentID = $contentID;
+      $stmt = $this->conn->prepare($sql);
+      $stmt->bind_param("i", $this->contentID);
+      $stmt->execute();
+      $results = $stmt->get_result();
+      if($results->num_rows == 1) {
+        $this->class = $results->fetch_assoc();
+      }
+    }
+  }
 ?>
