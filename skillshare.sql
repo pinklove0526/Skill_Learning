f9cc48f58f4497c532541d9ecaf6c963ef874ddd
Duong-Minh-Quang <1959032@itec.hcmus.edu.vn>
2022-04-10 09:42:37 +0700

    almost done the classroom view (rating bug)
    Tân ơi sửa hộ t vs

--- skillshare.sql
+++ skillshare.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 05, 2022 at 03:22 PM
+-- Generation Time: Mar 26, 2022 at 08:19 AM
 -- Server version: 8.0.21
 -- PHP Version: 7.3.21
 
@@ -30,47 +30,44 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `classroom`;
 CREATE TABLE IF NOT EXISTS `classroom` (
   `class_id` int NOT NULL AUTO_INCREMENT,
-  `TeacherID` int NOT NULL,
-  `class_type` text COLLATE utf8mb4_general_ci NOT NULL,
-  `info` text COLLATE utf8mb4_general_ci NOT NULL,
-  `class_name` text COLLATE utf8mb4_general_ci NOT NULL,
-  `contact_info` text COLLATE utf8mb4_general_ci NOT NULL,
-  `owner_name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
-  `video` text COLLATE utf8mb4_general_ci NOT NULL,
-  `Type` text COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`class_id`),
-  KEY `TeacherID` (`TeacherID`)
+  `class_type` text NOT NULL,
+  `info` text NOT NULL,
+  `class_name` text NOT NULL,
+  `contact_info` text NOT NULL,
+  `owner_name` text NOT NULL,
+  `video` text NOT NULL,
+  PRIMARY KEY (`class_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `classroom`
 --
 
-INSERT INTO `classroom` (`class_id`, `TeacherID`, `class_type`, `info`, `class_name`, `contact_info`, `owner_name`, `video`, `Type`) VALUES
-(1, 0, 'Martial arts', 'Learn to fight like Bruce Lee !!!', 'Fighting 101\r\n', '', '', 'videos/60e96b4401e3e.mp4', ''),
-(2, 0, 'Arts', 'Shading makes your character look better', 'Draw anime 101', '', '', 'videos/60e96b4401e3e.mp4', ''),
-(3, 0, 'Survival skills', 'Learn to survive in the wild', 'Live like Bear Grylls', '', '', 'videos/60e96b4401e3e.mp4', ''),
-(4, 0, 'Basic skills', 'Tips for life skills', 'How to basic', '', '', 'videos/60e96b4401e3e.mp4', ''),
-(5, 0, 'Arts', 'Digital art for beginners', 'Digital Arts', '', '', 'videos/60e96b4401e3e.mp4', ''),
-(6, 0, 'Survival skills', 'Upgrade water system', 'Better hygiene, better life', '', '', 'videos/60e96b4401e3e.mp4', ''),
-(7, 0, 'Martial Arts', 'How to control fear before and during the fight', 'Self denfence', '', '', 'videos/60e96b4401e3e.mp4', ''),
-(8, 0, 'Basic skills', 'Tips and tricks for beginner plumbers', 'Be a great plumber with Johnny Sins', '', '', 'videos/60e96b4401e3e.mp4', ''),
-(9, 0, 'Arts', 'Tips for character design', 'Design characters', '', '', 'videos/60e96b4401e3e.mp4', ''),
-(10, 0, 'Martial Arts', 'How to defend yourself when someone pushing you', 'Make your life safe', '', '', 'videos/60e96b4401e3e.mp4', ''),
-(17, 0, 'martial arts', 'dfasdfsdaf', 'fsdfas', '', '', 'videos/60e96b4401e3e.mp4', ''),
-(18, 0, 'martial arts', 'fasdfsdfsdfasdf', 'fasdfsdfads', '', '', 'videos/60e96b748c6c8.mp4', ''),
-(19, 0, 'martial arts', 'dsafsdfasdfsd', 'fsadfasdfasdf', '', '', 'videos/60e96bb6d0b97.mp4', ''),
-(20, 0, 'martial arts', 'asdfadsf', 'fsadfasdf', '', '', 'videos/60e96eb86f9db.mp4', ''),
-(21, 0, 'Basic skills', 'Tips for life skills', '', '', '', 'videos/60e96b4401e3e.mp4', ''),
-(22, 0, 'Basic skills', 'Tips for life skills', '', '', '', 'videos/60e96b4401e3e.mp4', ''),
-(23, 0, 'Basic skills', 'Tips for life skills', '', '', '', 'videos/60e96b4401e3e.mp4', ''),
-(24, 0, 'basic skills', 'az', 'asd', 'z', '', 'videos/61a5e99112c54.mp4', ''),
-(25, 0, 'survival skills', 'za', 'as', 'asxz', '', 'videos/61a5e9a065322.mp4', ''),
-(26, 0, 'survival skills', '0999999999', 'Survive', 'azaz', '', 'videos/61a62a8b53b6b.mp4', ''),
-(27, 0, 'martial arts', '01224245', 'martial', 'as', '', 'videos/61a62ad471920.mp4', ''),
-(28, 0, 'basic skills', '0218755123', 'damm', 'as', '', 'a', ''),
-(29, 0, 'talents', 'facebook', 'Bad', 'tesasz', '', 'a', ''),
-(30, 0, 'martial arts', 'insta', 'test2', 'asdsazxz', 'a', 'videos/61a62b988a2f4.mp4', '');
+INSERT INTO `classroom` (`class_id`, `class_type`, `info`, `class_name`, `contact_info`, `owner_name`, `video`) VALUES
+(1, 'Martial arts', 'Learn to fight like Bruce Lee !!!', 'Fighting 101\r\n', '', '', 'videos/60e96b4401e3e.mp4'),
+(2, 'Arts', 'Shading makes your character look better', 'Draw anime 101', '', '', 'videos/60e96b4401e3e.mp4'),
+(3, 'Survival skills', 'Learn to survive in the wild', 'Live like Bear Grylls', '', '', 'videos/60e96b4401e3e.mp4'),
+(4, 'Basic skills', 'Tips for life skills', 'How to basic', '', '', 'videos/60e96b4401e3e.mp4'),
+(5, 'Arts', 'Digital art for beginners', 'Digital Arts', '', '', 'videos/60e96b4401e3e.mp4'),
+(6, 'Survival skills', 'Upgrade water system', 'Better hygiene, better life', '', '', 'videos/60e96b4401e3e.mp4'),
+(7, 'Martial Arts', 'How to control fear before and during the fight', 'Self denfence', '', '', 'videos/60e96b4401e3e.mp4'),
+(8, 'Basic skills', 'Tips and tricks for beginner plumbers', 'Be a great plumber with Johnny Sins', '', '', 'videos/60e96b4401e3e.mp4'),
+(9, 'Arts', 'Tips for character design', 'Design characters', '', '', 'videos/60e96b4401e3e.mp4'),
+(10, 'Martial Arts', 'How to defend yourself when someone pushing you', 'Make your life safe', '', '', 'videos/60e96b4401e3e.mp4'),
+(17, 'martial arts', 'dfasdfsdaf', 'fsdfas', '', '', 'videos/60e96b4401e3e.mp4'),
+(18, 'martial arts', 'fasdfsdfsdfasdf', 'fasdfsdfads', '', '', 'videos/60e96b748c6c8.mp4'),
+(19, 'martial arts', 'dsafsdfasdfsd', 'fsadfasdfasdf', '', '', 'videos/60e96bb6d0b97.mp4'),
+(20, 'martial arts', 'asdfadsf', 'fsadfasdf', '', '', 'videos/60e96eb86f9db.mp4'),
+(21, 'Basic skills', 'Tips for life skills', '', '', '', 'videos/60e96b4401e3e.mp4'),
+(22, 'Basic skills', 'Tips for life skills', '', '', '', 'videos/60e96b4401e3e.mp4'),
+(23, 'Basic skills', 'Tips for life skills', '', '', '', 'videos/60e96b4401e3e.mp4'),
+(24, 'basic skills', 'az', 'asd', 'z', '', 'videos/61a5e99112c54.mp4'),
+(25, 'survival skills', 'za', 'as', 'asxz', '', 'videos/61a5e9a065322.mp4'),
+(26, 'survival skills', '0999999999', 'Survive', 'azaz', '', 'videos/61a62a8b53b6b.mp4'),
+(27, 'martial arts', '01224245', 'martial', 'as', '', 'videos/61a62ad471920.mp4'),
+(28, 'basic skills', '0218755123', 'damm', 'as', '', 'a'),
+(29, 'talents', 'facebook', 'Bad', 'tesasz', '', 'a'),
+(30, 'martial arts', 'insta', 'test2', 'asdsazxz', 'a', 'videos/61a62b988a2f4.mp4');
 
 -- --------------------------------------------------------
 
@@ -83,7 +80,7 @@ CREATE TABLE IF NOT EXISTS `comment` (
   `comment_id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `classroom_id` int NOT NULL,
-  `body` text COLLATE utf8mb4_general_ci NOT NULL,
+  `body` text NOT NULL,
   PRIMARY KEY (`comment_id`),
   KEY `comment_ibfk_1` (`classroom_id`),
   KEY `user_id` (`user_id`)
@@ -114,9 +111,9 @@ CREATE TABLE IF NOT EXISTS `material/quiz` (
   `ContentID` int NOT NULL AUTO_INCREMENT,
   `ClassroomID` int NOT NULL,
   `StudentID` int NOT NULL,
-  `ContentType` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `ContentInfo` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `ContentName` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `ContentType` text COLLATE utf8mb4_general_ci NOT NULL,
+  `ContentInfo` text COLLATE utf8mb4_general_ci NOT NULL,
+  `ContentName` text COLLATE utf8mb4_general_ci NOT NULL,
   `Chapter` int NOT NULL,
   `Score` float NOT NULL,
   PRIMARY KEY (`ContentID`),
@@ -156,7 +153,6 @@ CREATE TABLE IF NOT EXISTS `teacher` (
   `DOB` date NOT NULL,
   `Background` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `Avatar` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `Type` text COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`TeacherID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -169,10 +165,10 @@ CREATE TABLE IF NOT EXISTS `teacher` (
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `User_name` text COLLATE utf8mb4_general_ci NOT NULL,
-  `Email` text COLLATE utf8mb4_general_ci NOT NULL,
-  `hash` text COLLATE utf8mb4_general_ci NOT NULL,
-  `Avatar` text COLLATE utf8mb4_general_ci,
+  `User_name` text NOT NULL,
+  `Email` text NOT NULL,
+  `hash` text NOT NULL,
+  `Avatar` text,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `ID` (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -195,12 +191,6 @@ INSERT INTO `users` (`ID`, `User_name`, `Email`, `hash`, `Avatar`) VALUES
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `classroom`
---
-ALTER TABLE `classroom`
-  ADD CONSTRAINT `classroom_ibfk_1` FOREIGN KEY (`TeacherID`) REFERENCES `teacher` (`TeacherID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `comment`
